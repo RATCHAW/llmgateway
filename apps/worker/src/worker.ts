Third-Culture/tcs-llm-gateway

feat: report provider errors to discord

apps/worker/src/worker.ts
@@ -32,6 +32,10 @@ import {
 	refreshProjectHourlyStats,
 } from "./services/project-stats-aggregator.js";
 import {
+	notifyProviderError,
+	type ProviderErrorNotificationLog,
+} from "./services/provider-error-discord.js";
+import {
 	backfillHistoryIfNeeded,
 	calculateAggregatedStatistics,
 	calculateCurrentMinuteHistory,
@@ -119,6 +123,21 @@ const schema = z.object({
 	trace_id: z.string().nullable(),
 	unified_finish_reason: z.string().nullable(),
 });
+
+type ProcessedLogRow = z.infer<typeof schema>;
+
+const NOTIFIABLE_PROVIDER_ERROR_REASONS = new Set([
+	"gateway_error",
+	"upstream_error",
+]);
+
+function shouldNotifyProviderError(logRow: ProcessedLogRow): boolean {
+	return (
+		logRow.hasError === true &&
+		logRow.unified_finish_reason !== null &&
+		NOTIFIABLE_PROVIDER_ERROR_REASONS.has(logRow.unified_finish_reason)
+	);
+}
 
 export async function acquireLock(key: string): Promise<boolean> {
 	// eslint-disable-next-line no-mixed-operators
@@ -564,6 +583,8 @@ export async function batchProcessLogs(): Promise<void> {
 		return;
 	}
 
+	const providerErrorLogs: ProviderErrorNotificationLog[] = [];
+
 	try {
 		await db.transaction(async (tx) => {
 			// Get unprocessed logs with row-level locking to prevent concurrent processing
@@ -659,6 +680,26 @@ export async function batchProcessLogs(): Promise<void> {
 					traceId: row.trace_id,
 					unifiedFinishReason: row.unified_finish_reason,
 				});
+
+				const unifiedFinishReason = row.unified_finish_reason;
+
+				if (shouldNotifyProviderError(row) && unifiedFinishReason) {
+					providerErrorLogs.push({
+						duration: row.duration,
+						errorDetails: row.error_details,
+						logId: row.id,
+						organizationId: row.organization_id,
+						projectId: row.project_id,
+						requestId: row.request_id,
+						requestedModel: row.requested_model,
+						requestedProvider: row.requested_provider,
+						traceId: row.trace_id,
+						unifiedFinishReason,
+						usedModel: row.used_model,
+						usedModelMapping: row.used_model_mapping,
+						usedProvider: row.used_provider,
+					});
+				}
 
 				if (row.cost && row.cost > 0 && !row.cached) {
 					// Always update API key usage for non-cached logs with cost
@@ -825,6 +866,14 @@ export async function batchProcessLogs(): Promise<void> {
 
 			logger.debug(`Marked ${logIds.length} logs as processed`);
 		});
+
+		if (providerErrorLogs.length > 0) {
+			await Promise.all(
+				providerErrorLogs.map(async (providerErrorLog) => {
+					await notifyProviderError(providerErrorLog);
+				}),
+			);
+		}
 	} catch (error) {
 		logger.error(
 			"Error processing batch credit deductions",

RATCHAW/llmgateway

apps/worker/src/worker.ts
@@ -40,6 +40,10 @@ import {
 	refreshProjectHourlyStats,
 } from "./services/project-stats-aggregator.js";
 import {
+	notifyProviderError,
+	type ProviderErrorNotificationLog,
+} from "./services/provider-error-discord.js";
+import {
 	backfillHistoryIfNeeded,
 	calculateAggregatedStatistics,
 	calculateCurrentMinuteHistory,
@@ -196,6 +200,30 @@ const schema = z.object({
 	trace_id: z.string().nullable(),
 	unified_finish_reason: z.string().nullable(),
 });
+
+type ProcessedLogRow = z.infer<typeof schema>;
+
+const NOTIFIABLE_PROVIDER_ERROR_REASONS = new Set([
+	"gateway_error",
+	"upstream_error",
+]);
+
+function shouldNotifyProviderError(logRow: ProcessedLogRow): boolean {
+	if (logRow.hasError !== true) {
+		return false;
+	}
+
+	if (logRow.unified_finish_reason === "client_error") {
+		return false;
+	}
+
+	if (logRow.unified_finish_reason !== null) {
+		return NOTIFIABLE_PROVIDER_ERROR_REASONS.has(logRow.unified_finish_reason);
+	}
+
+	const statusCode = logRow.error_details?.statusCode;
+	return statusCode !== undefined && statusCode >= 500;
+}
 
 export async function acquireLock(key: string): Promise<boolean> {
 	// eslint-disable-next-line no-mixed-operators
@@ -650,6 +678,8 @@ export async function batchProcessLogs(): Promise<void> {
 	}
 
 	const deductedOrgIds: string[] = [];
+	const providerErrorLogs: ProviderErrorNotificationLog[] = [];
+	let shouldSendProviderErrorNotifications = false;
 
 	try {
 		await db.transaction(async (tx) => {
@@ -748,6 +778,24 @@ export async function batchProcessLogs(): Promise<void> {
 					traceId: row.trace_id,
 					unifiedFinishReason: row.unified_finish_reason,
 				});
+
+				if (shouldNotifyProviderError(row)) {
+					providerErrorLogs.push({
+						duration: row.duration,
+						errorDetails: row.error_details,
+						logId: row.id,
+						organizationId: row.organization_id,
+						projectId: row.project_id,
+						requestId: row.request_id,
+						requestedModel: row.requested_model,
+						requestedProvider: row.requested_provider,
+						traceId: row.trace_id,
+						unifiedFinishReason: row.unified_finish_reason,
+						usedModel: row.used_model,
+						usedModelMapping: row.used_model_mapping,
+						usedProvider: row.used_provider,
+					});
+				}
 
 				if (row.cost && row.cost > 0 && !row.cached) {
 					const apiKeyCost = new Decimal(row.cost);
@@ -952,11 +1000,7 @@ export async function batchProcessLogs(): Promise<void> {
 
 			logger.debug(`Marked ${logIds.length} logs as processed`);
 		});
-
-		// Async low-balance alert check (outside transaction, non-blocking)
-		if (deductedOrgIds.length > 0) {
-			void checkLowBalanceAlerts(deductedOrgIds);
-		}
+		shouldSendProviderErrorNotifications = true;
 	} catch (error) {
 		logger.error(
 			"Error processing batch credit deductions",
@@ -964,6 +1008,14 @@ export async function batchProcessLogs(): Promise<void> {
 		);
 	} finally {
 		await releaseLock(CREDIT_PROCESSING_LOCK_KEY);
+	}
+
+	if (deductedOrgIds.length > 0) {
+		void checkLowBalanceAlerts(deductedOrgIds);
+	}
+
+	if (shouldSendProviderErrorNotifications && providerErrorLogs.length > 0) {
+		await Promise.all(providerErrorLogs.map(notifyProviderError));
 	}
 }
 